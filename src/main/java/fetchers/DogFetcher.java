8df9569e58fe896e48b3cb3c633433cff58966d2
Mari Rødseth Haugen <h_mari@hotmail.com>
2020-11-27 09:34:02 +0100

    DogFetcher finished

--- src/main/java/fetchers/DogFetcher.java
+++ src/main/java/fetchers/DogFetcher.java
@@ -1,45 +1,55 @@
 package fetchers;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import dto.DogDTO;
+import dto.FunnyDTO;
 import dto.MemeDTO;
+import dto.YesOrNoDTO;
+import entities.Meme;
 import utils.HttpUtils;
-import java.lang.reflect.Type;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class DogFetcher {
 
-    private static String dogURL = "https://dog.ceo/api/breeds/image/random";
 
+    private static String dogURL = "https://dog.ceo/api/breeds/image/random";
     private static int numberOfTasks = 5;
 
     public static String fetchDog(ExecutorService threadpool, Gson gson) throws InterruptedException, ExecutionException, TimeoutException {
 
+        ArrayList<String> urls = new ArrayList<>();
+        for (int i = 0; i < numberOfTasks ; i++) {
+            urls.add(dogURL);
+        }
 
-        ArrayList<MemeDTO> tasks = new ArrayList<>();
+        ArrayList<Future<DogDTO>> futures = new ArrayList<>();
 
+        for (String url : urls) {
         Callable<DogDTO> dogTask = new Callable<DogDTO>() {
             @Override
             public DogDTO call() throws Exception {
-                String dog = HttpUtils.fetchData(dogURL);
-                Type listType = new TypeToken<List<DogDTO>>() {}.getType();
-
-                List<DogDTO> dogDTO = gson.fromJson(dog,listType);
-
-                return dogDTO.get(0);
-            }
+                String dog = HttpUtils.fetchData(url);
+                DogDTO dogDTO = gson.fromJson(dog, DogDTO.class);
+                return dogDTO;
+            }};
+            futures.add(threadpool.submit(dogTask));
         };
 
-        for (int i = 0; i < numberOfTasks; i++) {
-            Future<DogDTO> futureDog = threadpool.submit(dogTask);
-            DogDTO dog = futureDog.get(5, TimeUnit.SECONDS);
-            MemeDTO memeDTO = new MemeDTO(dog);
-            tasks.add(memeDTO);
+        List<DogDTO> result = new ArrayList<>();
+        for (Future<DogDTO> dtos: futures) {
+            result.add(dtos.get(2, TimeUnit.SECONDS));
+
         }
 
-        return gson.toJson(tasks);
+        List<MemeDTO> memeDTOS = new ArrayList<>();
+
+        for (DogDTO dtos: result) {
+            memeDTOS.add(new MemeDTO(dtos));
+        }
+
+        return gson.toJson(memeDTOS);
     }
 }
