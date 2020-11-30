5367ab71182c7de052ce1e9b785bc082ae92af23
Mari Rødseth Haugen <h_mari@hotmail.com>
2020-11-30 11:09:44 +0100

    startet on get FavouriteList

--- src/main/java/facades/MemeFacade.java
+++ src/main/java/facades/MemeFacade.java
@@ -1,5 +1,8 @@
 package facades;
 
+import dto.MemeDTO;
+import java.util.List;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 public class MemeFacade {
@@ -14,6 +17,15 @@ public class MemeFacade {
         }
         return instance;
     }
+    
+    public List<MemeDTO> getFavouriteMemes(String userName) {
+        EntityManager em = emf.createEntityManager();
+        List<MemeDTO> favourites;
+       
+        return favourites;
+    }
+    
+
 
     
 }
