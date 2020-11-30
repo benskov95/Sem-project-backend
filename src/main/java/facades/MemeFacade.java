ef4111b44ff8d0d4bf35ffd84e05c9169fe8972e
Matt Thomsen <cph-mt312@cphbusiness.dk>
2020-11-30 11:06:34 +0100

    added methods to get all upvoted + downvoted memes

--- src/main/java/facades/MemeFacade.java
+++ src/main/java/facades/MemeFacade.java
@@ -1,12 +1,18 @@
 package facades;
 
+import dto.MemeDTO;
+import entities.Meme;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 
 public class MemeFacade {
-    
+
     private static EntityManagerFactory emf;
     private static MemeFacade instance;
-    
+
     public static MemeFacade getMemeFacade(EntityManagerFactory _emf) {
         if (instance == null) {
             emf = _emf;
@@ -15,5 +21,33 @@ public class MemeFacade {
         return instance;
     }
 
+    public List<MemeDTO> getAllDownvotedMemes() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery query = em.createQuery("SELECT m FROM Meme m ORDER BY downvotes DESC", Meme.class);
+            List<Meme> memesList = query.getResultList();
+            List<MemeDTO> memeDTOsList = new ArrayList<>();
+            for (Meme meme : memesList) {
+                memeDTOsList.add(new MemeDTO(meme));
+            }
+            return memeDTOsList;
+        } finally {
+            em.close();
+        }
+    }
     
+    public List<MemeDTO> getAllUpvotedMemes() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery query = em.createQuery("SELECT m FROM Meme m ORDER BY upvotes DESC", Meme.class);
+            List<Meme> memesList = query.getResultList();
+            List<MemeDTO> memeDTOsList = new ArrayList<>();
+            for (Meme meme : memesList) {
+                memeDTOsList.add(new MemeDTO(meme));
+            }
+            return memeDTOsList;
+        } finally {
+            em.close();
+        }
+    }
 }
