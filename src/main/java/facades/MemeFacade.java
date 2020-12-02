a817563100e6784db4f96ff8a7a45fce36ef8900
Matt Thomsen <cph-mt312@cphbusiness.dk>
2020-12-02 10:36:33 +0100

    added getMemeById to MemeFacade

--- src/main/java/facades/MemeFacade.java
+++ src/main/java/facades/MemeFacade.java
@@ -1,6 +1,5 @@
 package facades;
 
-
 import dto.CommentDTO;
 import entities.Comment;
 import entities.Meme;
@@ -22,7 +21,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
 
-
 public class MemeFacade {
 
     private static EntityManagerFactory emf;
@@ -36,7 +34,6 @@ public class MemeFacade {
         return instance;
     }
 
-
     public CommentDTO addComment(CommentDTO commentDTO) {
 
         EntityManager em = emf.createEntityManager();
@@ -47,17 +44,17 @@ public class MemeFacade {
         meme.getComments().add(comment);
         comment.setMeme(meme);
 
-        try{
+        try {
             em.getTransaction().begin();
             em.persist(user);
             em.getTransaction().commit();
-        }finally {
+        } finally {
             em.close();
         }
         return new CommentDTO(comment);
     }
 
-    public List<CommentDTO> getAllCommentsById (int id) {
+    public List<CommentDTO> getAllCommentsById(int id) {
 
         EntityManager em = emf.createEntityManager();
 
@@ -67,18 +64,23 @@ public class MemeFacade {
         List<Comment> commentList = query.getResultList();
         List<CommentDTO> commentDTOList = new ArrayList<>();
 
-        for (Comment comment:commentList) {
+        for (Comment comment : commentList) {
             commentDTOList.add(new CommentDTO(comment));
         }
         return commentDTOList;
     }
 
+    public MemeDTO getMemeById(int id) {
+        EntityManager em = emf.createEntityManager();
+        Meme meme = em.find(Meme.class, id);
+        return new MemeDTO(meme);
+    }
 
     public MemeDTO upvoteMeme(String username, MemeDTO memeDTO) {
         EntityManager em = emf.createEntityManager();
         User user = em.find(User.class, username);
         Meme meme = checkIfMemeExists(memeDTO, em);
-        
+
         if (!checkHasUpvoted(meme, user, em)) {
             try {
                 em.getTransaction().begin();
@@ -96,16 +98,16 @@ public class MemeFacade {
         }
         return new MemeDTO(meme);
     }
-    
+
     public MemeDTO downvoteMeme(String username, MemeDTO memeDTO) {
         EntityManager em = emf.createEntityManager();
         User user = em.find(User.class, username);
         Meme meme = checkIfMemeExists(memeDTO, em);
-        
+
         if (!checkHasDownvoted(meme, user, em)) {
             try {
                 em.getTransaction().begin();
-                 if (user.getUpvotedMemes().contains(meme)) {
+                if (user.getUpvotedMemes().contains(meme)) {
                     meme.getUpvoters().remove(user);
                     user.getUpvotedMemes().remove(meme);
                 }
@@ -119,11 +121,11 @@ public class MemeFacade {
         }
         return new MemeDTO(meme);
     }
-    
+
     public Meme checkIfMemeExists(MemeDTO memeDTO, EntityManager em) {
         Query q = em.createQuery("SELECT m FROM Meme m WHERE m.imageUrl = :url");
         q.setParameter("url", memeDTO.getImageUrl());
-        
+
         if (q.getResultList().size() > 0) {
             return (Meme) q.getResultList().get(0);
         } else {
@@ -137,19 +139,19 @@ public class MemeFacade {
             return meme;
         }
     }
-    
+
     public boolean checkHasUpvoted(Meme meme, User user, EntityManager em) {
         if (user.getUpvotedMemes().contains(meme)) {
             em.getTransaction().begin();
             user.getUpvotedMemes().remove(meme);
             meme.getUpvoters().remove(user);
             em.getTransaction().commit();
-            return true;   
+            return true;
         } else {
             return false;
-        }  
+        }
     }
-    
+
     public boolean checkHasDownvoted(Meme meme, User user, EntityManager em) {
         if (user.getDownvotedMemes().contains(meme)) {
             em.getTransaction().begin();
@@ -176,7 +178,7 @@ public class MemeFacade {
             em.close();
         }
     }
-    
+
     public List<MemeDTO> getAllUpvotedMemes() {
         EntityManager em = emf.createEntityManager();
         try {
@@ -191,14 +193,14 @@ public class MemeFacade {
             em.close();
         }
     }
-    
-       public List<MemeDTO> getFavoriteMemes(String userName) {
+
+    public List<MemeDTO> getFavoriteMemes(String userName) {
         EntityManager em = emf.createEntityManager();
-        try{
-        TypedQuery<Meme> query = em.createQuery("SELECT m FROM Meme m join m.upvoters u WHERE u.username = :username", Meme.class);
-        query.setParameter("username", userName);
-        List<Meme> result = query.getResultList();
-        List<MemeDTO> memeDTOsList = new ArrayList<>();
+        try {
+            TypedQuery<Meme> query = em.createQuery("SELECT m FROM Meme m join m.upvoters u WHERE u.username = :username", Meme.class);
+            query.setParameter("username", userName);
+            List<Meme> result = query.getResultList();
+            List<MemeDTO> memeDTOsList = new ArrayList<>();
             for (Meme meme : result) {
                 memeDTOsList.add(new MemeDTO(meme));
             }
@@ -207,6 +209,5 @@ public class MemeFacade {
             em.close();
         }
     }
-    
 
 }
